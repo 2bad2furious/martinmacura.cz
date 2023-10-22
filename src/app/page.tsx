6c180244f3619ac72c251d1869867a02f660dd33
2bad2furious <22004964+2bad2furious@users.noreply.github.com>
2023-10-22 07:23:36 +0200

    :lipstick: Gap, padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { getInitialTitle } from '~/server';
 
 export default async function Home() {
     return (
-        <Stack mih='100vh' align='center' justify='center'>
+        <Stack mih='100vh' align='center' justify='center' gap='xl' p='md'>
             <Title initial={await getInitialTitle()} />
 
             <Affix position={{ bottom: '2rem', right: '2rem' }} visibleFrom='md'>
